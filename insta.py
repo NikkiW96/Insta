6b618e35d56b510303ea15557aa93687eecc7204
Nikita Wadhwani <43293583+NikkiW96@users.noreply.github.com>
2020-07-18 20:20:55 +0530

    Add files via upload

---
+++ insta.py
@@ -0,0 +1,7 @@
+#DOWNLOADING INSTAGRAM POSTS
+#WORKS ONLY ON PUBLIC ACCOUNT
+
+import webbrowser
+post_url=input("Enter your instagram post url: ")
+instaurl="savefrom.net/"+post_url
+webbrowser.open(instaurl)
